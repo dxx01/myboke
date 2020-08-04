292310c133ce1c0f6958dc198865b9a289117955
Mr.Deng <1693172461@qq.com>
2020-08-04 23:31:35 +0800

    demo

---
+++ src/views/header/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="header">
+    <div class="logo">去见她</div>
+    header
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header",
+  props: [""],
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {}
+};
+</script>
+<style lang="scss" scoped>
+#header {
+  width: 100%;
+  height: 100%;
+  .logo {
+    font-size: 5em;
+  }
+}
+</style>
