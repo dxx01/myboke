8858de4481bec9c7e6ef1f6d46c350fb2a2f81a8
Mr.Deng <1693172461@qq.com>
2020-08-07 21:39:25 +0800

    首页完善中

--- src/views/header/index.vue
+++ src/views/header/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="headerMode">
+    <!-- 大屏模式展示(>650px) -->
     <div class="nav">
       <div class="logo">邓鑫鑫博客</div>
       <div class="navlist">
@@ -9,6 +10,85 @@
             v-for="(item, index) in navData"
             :key="index"
             @click="choose(item.path)"
+          >
+            {{ item.name }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- 小屏模式展示(<650px) -->
+    <div class="mobile_nav">
+      <div class="logo">邓鑫鑫博客</div>
+      <div class="mobile_menu" @click="changeIcon(!iconShiow)">
+        <!-- 三 -->
+        <svg
+          v-if="iconShiow"
+          t="1596722270456"
+          class="icon"
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          p-id="15126"
+          width="20"
+          height="20"
+          style="margin-top:4px;"
+        >
+          <path
+            d="M867.995 459.647h-711.99c-27.921 0-52.353 24.434-52.353 52.353s24.434 52.353 52.353 52.353h711.99c27.921 0 52.353-24.434 52.353-52.353s-24.434-52.353-52.353-52.353z"
+            p-id="15127"
+            fill="#777777"
+          ></path>
+          <path
+            d="M867.995 763.291h-711.99c-27.921 0-52.353 24.434-52.353 52.353s24.434 52.353 52.353 52.353h711.99c27.921 0 52.353-24.434 52.353-52.353s-24.434-52.353-52.353-52.353z"
+            p-id="15128"
+            fill="#777777"
+          ></path>
+          <path
+            d="M156.005 260.709h711.99c27.921 0 52.353-24.434 52.353-52.353s-24.434-52.353-52.353-52.353h-711.99c-27.921 0-52.353 24.434-52.353 52.353s24.434 52.353 52.353 52.353z"
+            p-id="15129"
+            fill="#777777"
+          ></path>
+        </svg>
+        <!-- x -->
+        <svg
+          v-if="!iconShiow"
+          t="1596724076285"
+          class="icon"
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          p-id="19415"
+          width="16"
+          height="16"
+          style="margin-top:6px;"
+        >
+          <path
+            d="M768 341.344L682.656 256 512 426.656 341.344 256 256 341.344 426.656 512 256 682.656 341.344 768 512 597.344 682.656 768 768 682.656 597.344 512z"
+            p-id="19416"
+            fill="#777777"
+          ></path>
+        </svg>
+      </div>
+      <!-- 小屏模式菜单列表(<650px) -->
+      <div class="mobile_dropdown" v-if="!iconShiow">
+        <!-- 搜索 -->
+        <div class="mobile_search">
+          <input
+            type="text"
+            placeholder="请输入搜索内容"
+            class="mobile_inputSearch"
+            autocomplete="off"
+          />
+          <div class="mobile_searchBtn" @click="changeIcon(!iconShiow)">
+            搜索
+          </div>
+        </div>
+        <ul>
+          <li
+            :class="active === item.path ? 'active' : ''"
+            v-for="(item, index) in navData"
+            :key="index"
+            @click="choose(item.path, !iconShiow)"
           >
             {{ item.name }}
           </li>
@@ -51,7 +131,10 @@ export default {
           path: "about"
         }
       ],
-      active: "shouye"
+      // 当前选中
+      active: "shouye",
+      // 小屏模式下图标切换
+      iconShiow: true
     };
   },
 
@@ -61,12 +144,23 @@ export default {
 
   beforeMount() {},
 
-  mounted() {},
+  mounted() {
+    // 每次缩小到小屏都重置iconShiow
+    window.onresize = () => {
+      if (document.body.clientWidth < 650) this.iconShiow = true;
+    };
+  },
 
   methods: {
     // 选择头部标签
     choose(path) {
       this.active = path;
+      // 小屏时点击菜单选项下拉消失
+      if (document.body.clientWidth < 650) this.iconShiow = !this.iconShiow;
+    },
+    // 小屏模式下切换展开图标
+    changeIcon(val) {
+      this.iconShiow = val;
     }
   },
 
@@ -89,12 +183,16 @@ export default {
     height: 100%;
     display: flex;
     justify-content: space-between;
+    @media (max-width: 650px) {
+      display: none;
+    }
     .logo {
       font-size: 22px;
       font-family: cursive;
       font-weight: bolder;
       margin-left: 10px;
       line-height: 60px;
+      cursor: pointer;
     }
     .navlist {
       color: #777;
@@ -104,7 +202,7 @@ export default {
         list-style-type: none;
         display: flex;
         flex-direction: row;
-        @media (max-width: 550px) {
+        @media (max-width: 650px) {
           display: none;
         }
         li {
@@ -122,6 +220,90 @@ export default {
         li:hover {
           background-color: #4ba4ff;
           color: #fff;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+  .mobile_nav {
+    position: relative;
+    top: 0;
+    display: flex;
+    justify-content: space-between;
+    @media (min-width: 650px) {
+      display: none;
+    }
+    .logo {
+      font-size: 22px;
+      font-family: cursive;
+      font-weight: bolder;
+      margin-left: 10px;
+      line-height: 60px;
+      cursor: pointer;
+    }
+    .mobile_menu {
+      width: 28px;
+      height: 28px;
+      background-color: rgb(248, 248, 248);
+      cursor: pointer;
+      border-radius: 4px;
+      border-width: 1px;
+      border-style: solid;
+      border-color: rgb(238, 238, 238);
+      text-align: center;
+      margin: 13px 20px 0 0;
+    }
+    .mobile_dropdown {
+      position: absolute;
+      top: 60px;
+      width: 100%;
+      z-index: 2;
+      background-color: rgba(44, 62, 80, 0.2);
+      ul,
+      li {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        text-align: center;
+        padding: 10px 0;
+        cursor: pointer;
+        color: #777;
+      }
+      .active {
+        color: #4ba4ff;
+      }
+      li:hover {
+        background-color: rgba(255, 255, 255, 1);
+      }
+      .mobile_search {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        height: 40px;
+        .mobile_inputSearch {
+          width: 90%;
+          padding: 5px 12px;
+          color: #777;
+          background-color: #fff;
+          font-size: 15px;
+          box-shadow: none;
+          border: 2px solid #ccc;
+          display: table-cell;
+          // 去除选中的边框颜色
+          outline: none;
+        }
+        .mobile_searchBtn {
+          text-align: center;
+          line-height: 40px;
+          height: 40px;
+          width: 10%;
+          color: white;
+          font-size: 14px;
+          font-weight: 400;
+          background-color: #ccc;
+        }
+        .mobile_searchBtn:hover {
+          color: #409eff;
           cursor: pointer;
         }
       }
