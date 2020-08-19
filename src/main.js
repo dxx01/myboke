aa836111c90b4f2fbb2a3fbf2d152f7bd1af97ad
Mr.deng <1693172461@qq.com>
2020-08-19 23:18:33 +0800

    genxin

--- src/main.js
+++ src/main.js
@@ -3,16 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ElementUI from "element-ui";
-//swiper 轮播插件
-import VueAwesomeSwiper from "vue-awesome-swiper";
 
-import "swiper/swiper-bundle.css";
 import "element-ui/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
-Vue.use(VueAwesomeSwiper);
 
 new Vue({
   router,
--- src/views/container/content/shouye.vue
+++ src/views/container/content/shouye.vue
@@ -1,26 +1,43 @@
 <template>
   <div id="shouye">
     <div class="left">
-      <Carousel />
+      <!-- 轮播 -->
+      <div class="sy_img">
+        <el-carousel indicator-position="none">
+          <el-carousel-item v-for="item in imgNum" :key="item">
+            <el-image
+              fit="cover"
+              style="width: 100%; height: 100%"
+              :src="require('@/assets/imgs/lunbo/' + item + '.jpg')"
+            />
+          </el-carousel-item>
+        </el-carousel>
+        <!-- 图片配文 -->
+        <div class="sy-font">我与我周旋久，宁做我！</div>
+      </div>
     </div>
     <div class="right"></div>
   </div>
 </template>
 
 <script>
-import Carousel from "@/components/carousel.vue";
 export default {
   name: "shouye",
   data() {
-    return {};
-  },
-  components: {
-    Carousel
+    return {
+      imgNum: 6
+    };
   }
 };
 </script>
 
 <style lang="scss" scoped>
+//外部样式字体文件
+@font-face {
+  font-family: lunbo-font;
+  src: url("~@/assets/font/lunbo-font.ttf");
+  font-weight: bold;
+}
 #shouye {
   margin: 0 auto;
   max-width: 1200px;
@@ -32,6 +49,22 @@ export default {
     background: red;
     @media (max-width: 900px) {
       width: 100%;
+    }
+
+    .sy_img {
+      position: relative;
+      width: 100%;
+      height: 300px;
+
+      .sy-font {
+        font-family: lunbo-font;
+        position: absolute;
+        z-index: 2;
+        bottom: 10px;
+        left: 20px;
+        font-size: 30px;
+        color: #5b81fe;
+      }
     }
   }
   .right {
